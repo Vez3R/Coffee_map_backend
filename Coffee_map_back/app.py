410df15939ed93ba414c44f43536ac44b771bf19
Vlad <t.vladkuznetsov@gmail.com>
2024-12-09 20:10:44 +0300

    Создание файла с зависимостями, описание модели таблицы users, создание тестовой выгрузки

--- Coffee_map_back/app.py
+++ Coffee_map_back/app.py
@@ -6,5 +6,27 @@ from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/postgres"
 db = SQLAlchemy(app)
+
+class UsersModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    login = db.Column(db.Text())
+    password = db.Column(db.Text())
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        
+@app.route('/', methods=['GET'])
+def start():
+    records = UsersModel.query.all()
+    results = [
+        {
+            "login": record.login,
+            "password": record.password
+        } for record in records]
+
+    return json.dumps(results, default=str)
